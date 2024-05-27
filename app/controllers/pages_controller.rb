eb0d471c0e3c0770993153c68b610d4707e8e8b1
Henrique-1992 <hleitaonunes@gmail.com>
2024-05-27 16:23:06 +0100

    Added FirstNameLastName

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
   def home
   end
 end
