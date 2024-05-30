135e934f10667fdf666f675a8c3c04fd9f8bae40
Uzoma Agha <u.agha@outlook.com>
2024-05-30 14:09:50 +0100

    Create new killer, add specialty

--- app/models/killer.rb
+++ app/models/killer.rb
@@ -3,6 +3,6 @@ class Killer < ApplicationRecord
   has_many_attached :photos
   has_many :users, through: :bookings
   belongs_to :user
-  validates :first_name, :last_name, :description, presence: true
+  validates :first_name, :last_name, :description, :specialty, presence: true
   validates :description, length: { minimum: 6 }
 end
